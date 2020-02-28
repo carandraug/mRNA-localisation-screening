8dbceb1e4537543ea1e7116790ece5bd346a0d65
Carnë Draug <carandraug+dev@gmail.com>
2020-02-28 11:34:15 +0000

    questionnaire.py: enable questions with text box answer and different radio buttons
    This changes the format of the questions file which now must be python code.
    Add documentation with the file format.

--- src/questionnaire.py
+++ src/questionnaire.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-## Copyright (C) 2018 David Pinto <david.pinto@bioch.ox.ac.uk>
+## Copyright (C) 2020 David Pinto <david.pinto@bioch.ox.ac.uk>
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -16,67 +16,161 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+## SYNOPSIS
+##   questionnaire QUESTIONS-FPATH SAVE-DIR [IMG-FPATHS ...]
+##
+## FORMAT OF QUESTIONS FILE
+##
+##   The questions file is a python file with a QUESTIONS variable.
+##   This QUESTIONS variable must be a list, of tuples with two
+##   elements.  Each tuple defines an individual question.
+##
+##   The first element in the question tuple must be a string and is
+##   the text for the actual question.  The second element in the
+##   tuple specifies the answer type and its initial value: if it is a
+##   string then the answer is a text box; if it is a tuple of strings
+##   then it is a group of radio buttons.
+
+import argparse
+import collections.abc
+import importlib
 import os.path
 import pickle
 import subprocess
 import sys
+import typing
 
 from PyQt5 import QtWidgets
 
 
-LABELS = ('yes', 'no', 'unsure')
-DEFAULT_LABEL = 'unsure'
+def read_questions(filepath: str) -> typing.Sequence[typing.Tuple]:
+    spec = importlib.util.spec_from_file_location('questions', filepath)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    try:
+        questions = getattr(module, 'QUESTIONS', None)
+    except AttributeError:
+        raise ValueError('could not find QUESTIONS on file %s' % filepath)
+    return questions
+
+
+def validate_questions(questions) -> None:
+    if not isinstance(questions, collections.abc.Sequence):
+        raise ValueError('QUESTIONS must be a sequence')
+    for question in questions:
+        if not isinstance(question, tuple) and len(question) != 2:
+            raise ValueError('each QUESTIONS element must be a tuple of 2')
+        question_text = question[0]
+        answer_spec = question[1]
+        if not isinstance(question_text, str):
+            raise ValueError('question text must be a str')
+        if not isinstance(answer_spec, (str, tuple)):
+            raise ValueError('answers for a question must be a str or tuple')
+        if (isinstance(answer_spec, tuple)
+            and not all([isinstance(x, str) for x in answer_spec])):
+            raise ValueError('if answer is a tuple, all elements must be str')
+
+
+class QuestionWidget(QtWidgets.QWidget):
+    def __init__(self, question: str, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._question = QtWidgets.QLabel(text=question, parent=self)
+
+    def to_serializable(self) -> typing.Tuple[str, str]:
+        """Two element tuple with question and answer text"""
+        raise NotImplementedError()
+
+    def reset(self) -> None:
+        raise NotImplementedError()
+
+class SelectQuestion(QuestionWidget):
+    """QuestionWidget for group of radio buttons."""
+    def __init__(self, question: str, options: typing.Sequence[str],
+                 *args, **kwargs) -> None:
+        super().__init__(question, *args, **kwargs)
+        self._answer = QtWidgets.QButtonGroup(parent=self)
+
+        for i, option in enumerate(options):
+            button = QtWidgets.QRadioButton(text=option, parent=self)
+            # By default, select first option.  We need to
+            # start with one select otherwise we may end in a
+            # state where none is selected.
+            if i == 0:
+                button.setChecked(True)
+            self._answer.addButton(button)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self._question)
+        button_box = QtWidgets.QHBoxLayout()
+        for button in self._answer.buttons():
+            button_box.addWidget(button)
+        button_box.addStretch() # left align buttons
+        layout.addLayout(button_box)
+        self.setLayout(layout)
+
+    def to_serializable(self) -> typing.Tuple[str, str]:
+        return (self._question.text(),
+                self._answer.checkedButton().text())
+
+    def reset(self) -> None:
+        self._answer.buttons()[0].setChecked(True)
+
+class TextQuestion(QuestionWidget):
+    """QuestionWidget for text box."""
+    def __init__(self, question: str, start_text: str = '',
+                 *args, **kwargs) -> None:
+        super().__init__(question, *args, **kwargs)
+        self._answer = QtWidgets.QPlainTextEdit(start_text, parent=self)
+        self._start_text = start_text
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self._question)
+        layout.addWidget(self._answer)
+        self.setLayout(layout)
+
+    def to_serializable(self) -> typing.Tuple[str, str]:
+        return (self._question.text(),
+                self._answer.toPlainText())
+
+    def reset(self) -> None:
+        self._answer.setPlainText(self._start_text)
+
 
 class Questionnaire(QtWidgets.QWidget):
-    default_label_idx = LABELS.index(DEFAULT_LABEL)
-
     def __init__(self, questions, *args, **kwargs):
-        super(Questionnaire, self).__init__(*args, **kwargs)
-        self.questions = questions
-        self.answers = [None] * len(questions)
-        self.set_ui()
+        super().__init__(*args, **kwargs)
 
-    def set_ui(self):
-        grid_box = QtWidgets.QGroupBox()
-        grid = QtWidgets.QGridLayout()
+        self._questions = []
+        for (question, answer_spec) in questions:
+            if isinstance(answer_spec, tuple):
+                question_widget = SelectQuestion
+            elif isinstance(answer_spec, str):
+                question_widget = TextQuestion
+            else:
+                raise ValueError('don\'t know what to do with answer %s'
+                                 % str(answer_spec))
+            self._questions.append(question_widget(question, answer_spec))
 
-        ## Header
-        for i, label in enumerate(LABELS):
-            grid.addWidget(QtWidgets.QLabel(label), 0, i+1)
-
-        ## Questions/Answers
-        for i, question in enumerate(self.questions):
-            grid.addWidget(QtWidgets.QLabel(question), i+1, 0)
-
-            self.answers[i] = QtWidgets.QButtonGroup(parent=self)
-            for j in range(len(LABELS)):
-                button = QtWidgets.QRadioButton(parent=self)
-                if j == Questionnaire.default_label_idx:
-                    button.setChecked(True)
-                ## Manually set id with the index for labels list.
-                self.answers[i].addButton(button, id=j)
-                grid.addWidget(button, i+1 , j+1)
-
-        self.setLayout(grid)
+        layout = QtWidgets.QVBoxLayout()
+        for question in self._questions:
+            layout.addWidget(question)
+        self.setLayout(layout)
 
     def to_serializable(self):
-        results = dict()
-        for question, answer in zip(self.questions, self.answers):
-            results[question] = LABELS[answer.checkedId()]
-        return results
+        return [q.to_serializable() for q in self._questions]
 
     def reset(self):
-        for answer in self.answers:
-            answer.button(Questionnaire.default_label_idx).setChecked(True)
+        for question in self._questions:
+            question.reset()
 
 
 class QuestionWindow(QtWidgets.QWidget):
-    def __init__(self, questions, answers_dir, img_fpaths,
+    def __init__(self, questions, save_dir, img_fpaths,
                  *args, **kwargs):
-        super(QuestionWindow, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.setWindowTitle('Blind Questions')
 
-        self.answers_dir = answers_dir
+        self.save_dir = save_dir
         self.img_fpaths = img_fpaths
         self.current_img = 0
 
@@ -96,7 +190,6 @@ class QuestionWindow(QtWidgets.QWidget):
         layout.addWidget(self.questionnaire)
         layout.addWidget(save_button)
         self.setLayout(layout)
-        print("list is ", self.img_fpaths)
 
     def open_image(self):
         fig_file = self.img_fpaths[self.current_img]
@@ -110,13 +203,23 @@ class QuestionWindow(QtWidgets.QWidget):
     def save_and_next(self):
         fig_file = self.img_fpaths[self.current_img]
         fname = os.path.splitext(os.path.basename(fig_file))[0]
-        fpath = os.path.join(self.answers_dir, fname + '.pickle')
+        fpath = os.path.join(self.save_dir, fname + '.pickle')
         if os.path.exists(fpath):
-            raise RuntimeError('file already exists, do not overwrite results')
+            error_dialog = QtWidgets.QErrorMessage(parent=self)
+            error_dialog.setModal(True)
+            error_dialog.showMessage('A file named \'%s\' for this image'
+                                     ' answers already exists.  Doing nothing'
+                                     ' until that is resolved' % fpath)
+            error_dialog.exec_();
+            return
+
         with open(fpath, 'wb') as fh:
             pickle.dump(self.questionnaire.to_serializable(), fh)
-        self.viewer.terminate()
 
+        # Close the image viewer to prevent situation where the users
+        # ends up with more than one image to score open and
+        # accidentally scores the wrong one.
+        self.viewer.terminate()
         self.next_image()
 
     def next_image(self):
@@ -130,16 +233,35 @@ class QuestionWindow(QtWidgets.QWidget):
             QtWidgets.qApp.quit()
 
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+def parse_arguments(arguments):
+    parser = argparse.ArgumentParser(prog='mRNA loc questionnaire')
+    parser.add_argument('questions_fpath', action='store', type=str,
+                        help='Filepath for python file with questions')
+    parser.add_argument('save_dir', action='store', type=str,
+                        help='Directory where to save the answers')
+    parser.add_argument('img_fpaths', action='store', type=str, nargs='+',
+                        help='Image files to make questions about')
+    args = parser.parse_args(arguments[1:])
+    if not os.path.isfile(args.questions_fpath):
+        raise ValueError('no file \'%s\' for questions' % args.questions_fpath)
+    if not os.path.isdir(args.save_dir):
+        raise ValueError('no dir \'%s\' to save answers' % args.save_dir)
+    for img_fpath in args.img_fpaths:
+        if not os.path.isfile(img_fpath):
+            raise ValueError('no img file \'%s\'' % img_fpath)
+    return args
 
-    questions_fpath = sys.argv[1]
-    questions = [l.strip() for l in open(questions_fpath, 'r').readlines()]
 
-    answers_dir = sys.argv[2]
+def main(argv):
+    app = QtWidgets.QApplication(argv)
 
-    img_fpaths = sys.argv[3:]
+    args = parse_arguments(app.arguments())
+    questions = read_questions(args.questions_fpath)
+    validate_questions(questions)
 
-    window = QuestionWindow(questions, answers_dir, img_fpaths)
+    window = QuestionWindow(questions, args.save_dir, args.img_fpaths)
     window.show()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main(sys.argv)
